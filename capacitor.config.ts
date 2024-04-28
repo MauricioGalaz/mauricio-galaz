50f71aced989c002c4b944e4aad7f57380c9a715
Mauricio Galaz <mgescalibur@gmail.com>
2024-04-28 15:30:30 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mauricio.galaz',
+  appName: 'mauricio-galaz',
+  webDir: 'www'
+};
+
+export default config;
